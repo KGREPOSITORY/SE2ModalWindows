2718d8be54d38eacf1d8120c3c42e601ab26f99c
KGfor <kg2forwork@gmail.com>
2020-10-28 21:09:46 +0300

    еуые 123

--- src/test/java/Settings.java
+++ src/test/java/Settings.java
@@ -7,7 +7,7 @@ public class Settings {
     protected ChromeDriver driver;
 
     public void setupConfig(){
-        System.setProperty("webdriver.chrome.driver","D:\\Новая папка\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver","src/test/resources/chromedriver.exe");
         driver  = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.MINUTES);
         driver.manage().timeouts().pageLoadTimeout(4, TimeUnit.MINUTES);
